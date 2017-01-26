1c2c5b80643928db2b00baac7af290f5e4829e9a
jwright <jwright@192.168.56.1>
2017-01-26 10:45:03 -0500

    Added the HourlyEmployee and CommissionEmployee classes

--- src/w17jan25part2/W17Jan25part2.java
+++ src/w17jan25part2/W17Jan25part2.java
@@ -18,17 +18,33 @@ public class W17Jan25part2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Employee empl1 = new Employee("Fred","Flintstone", 1234, 
-                                       LocalDate.of(1956, Month.MARCH, 12));
+        HourlyEmployee empl1 = new HourlyEmployee("Fred","Flintstone", 1234, 
+                                       LocalDate.of(1956, Month.MARCH, 12),23.12);
         
         System.out.printf("The first employee is %s%n", empl1.toString());
         
-        Employee empl2 = new Employee("Barney","Rubble", 1234, 
-                                       LocalDate.of(1958, Month.JANUARY, 1));
+        HourlyEmployee empl2 = new HourlyEmployee("Barney","Rubble", 1234, 
+                                       LocalDate.of(1958, Month.JANUARY, 1),15.87);
         
         System.out.printf("The second employee is %s%n", empl2.toString());
         
+        empl1.addHoursWorked(13);
+        empl1.addHoursWorked(4);
+        empl2.addHoursWorked(10);
+        empl2.addHoursWorked(10);
+        
+        System.out.printf("The paycheque for %s is %s%n", empl1, empl1.getPayCheque());
+        System.out.printf("The paycheque for %s is %s%n", empl2, empl2.getPayCheque());
+        
+        CommissionEmployee empl3 = new CommissionEmployee("Bam Bam","Rubble", 1234, 
+                                       LocalDate.of(1972, Month.JANUARY, 1), 5);
+        
+        empl3.addSales(3456.32);
+        empl3.addSales(456.88);
+        empl3.addSales(17456.32);
+        
+        System.out.printf("PayCheque for employee 3: %s%n", empl3.getPayCheque());
+        
         
     }
-    
 }
